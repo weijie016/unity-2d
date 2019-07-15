4846ea6c13d3c47b3be908a3bd7cd2d34a420abe
weijie016 <vege_wei_jie@hotmail.com>
2019-07-15 21:48:40 +0800

    7/15 1

---
+++ udemy_2D/laser_defender/Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+	// config params
+	[SerializeField] WaveConfig waveConfig;
+	[SerializeField] float moveSpeed = 3f;
+
+	// state
+	List<Transform> waypoints;
+	int waypointIndex = 0;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		waypoints = waveConfig.GetWaypoints();
+		transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		Move();
+	}
+
+	private void Move()
+	{
+		if (waypointIndex <= waypoints.Count - 1)
+		{
+			var targetPos = waypoints[waypointIndex].position;
+			var movementThisFrame = moveSpeed * Time.deltaTime;
+			transform.position = Vector2.MoveTowards(transform.position, targetPos, movementThisFrame);
+			if (transform.position == targetPos)
+			{
+				waypointIndex++;
+			}
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ udemy_2D/laser_defender/Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+	[SerializeField] List<WaveConfig> waveConfigs;
+
+	int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		var currWave = waveConfigs[startingWave];
+		StartCoroutine(SpawnAllEnemiesInWave(currWave));
+    }
+
+	private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+	{
+		for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+		{
+			Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].position, Quaternion.identity);
+			yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+		}
+	}
+}
